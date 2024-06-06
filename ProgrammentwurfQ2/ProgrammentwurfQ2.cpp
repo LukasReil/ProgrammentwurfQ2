c51c22178310212878b0b57735ad932e9c1abd32
Lukas Reil <lukas_reil0305@gmx.net>
2024-06-06 11:54:42 +0200

    Projektdateien hinzufügen.

---
+++ ProgrammentwurfQ2/ProgrammentwurfQ2.cpp
@@ -0,0 +1,12 @@
+﻿// ProgrammentwurfQ2.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "ProgrammentwurfQ2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
